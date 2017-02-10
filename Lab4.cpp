28419737685c171be3a6d0b96fc1a34bae243231
ChrizzFeller <chrizzhenriquez@unitec.edu>
2017-02-10 14:06:54 -0600

    Trabajando.

--- Lab4.cpp
+++ Lab4.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 using namespace std;
-
+	
 main() {
 
+	String word1, word2;
+	cout << "Ingrese la primera palabra." << endl;
+	cin >> word1;
+	cout << "Ingrese la segunda palabra." << endl;
+	cin >> word2;
+	//Metodo 1
+	
 
 
-//-------------------------
+	//-------------------------
 
 
 
