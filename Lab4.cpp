7ea8839dc621ec1e7add5546fbb2712f1de8259e
ChrizzFeller <chrizzhenriquez@unitec.edu>
2017-02-10 17:28:31 -0600

    Primer metodo terminado.

--- Lab4.cpp
+++ Lab4.cpp
@@ -12,11 +12,11 @@ main() {
 	cout << "Ingrese la segunda palabra." << endl;
 	cin >> word2;
 	//Metodo 1
-	int num1, num2, cont = 0;
+	int num1, num2, cont = 0, X, Y;
 	num1 = word1.length();
-	num2 = word2.length();
 	char arr2[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-        char* arr1 = arr2;
+        char arr3[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+	char* arr1 = arr2;
 	char** mat = new char*[26];
 	for(int i = 0; i < 26; i++) {
         	mat[i] = new char[26];
@@ -29,10 +29,31 @@ main() {
 		}
 		arr1 = cambio(arr1);
         }
+	//arr1 = cambio(arr1);
+	char temp, temp1;
+	int cont1 = 0, cont2 = 0;
+	for(int i = 0; i < word2.length(); i++) {
+		if (word1[i] != ' ') {
+			temp = word1[cont1];
+			temp1 = word2[cont2];
+			for (int j = 0; j < 26; j++) {
+				if (temp == arr2[j]) {
+					X = j;	
+				}
+				if (temp1 == arr2[j]) {
+					Y = j;
+				}
+			}
+			word2[i] = mat[X][Y];
+		}
+		cont1++;
+		cont2++;
+		if (cont1 == num1) {
+			cont1 = 0;
+		}
+	}
 	
-	
-
-
+	cout << word2 << endl;
 	//-------------------------
 
 
@@ -52,8 +73,8 @@ char* cambio(char* arr) {
         arr[25] = temp2;
         return arr;
 }
-void imprimir(char**){
-for (int i = 0; i < 26; i++) u{
+void imprimir(char** mat){
+	for (int i = 0; i < 26; i++) {
                 for(int j = 0; j < 26; j++) {
                         cout << "[" << mat[i][j] << "] ";
                 }
